fd5a775c7d1ca2961eda8b92deae6ea87ed1cb13
finn-01 <nguyensam576@gmail.com>
2022-02-14 11:11:49 +0700

    use @Configuration

--- src/main/java/vn/finn/spring/sb6/Sb6Application.java
+++ src/main/java/vn/finn/spring/sb6/Sb6Application.java
@@ -2,12 +2,18 @@ package vn.finn.spring.sb6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Sb6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sb6Application.class, args);
+
+        ApplicationContext context = SpringApplication.run(Sb6Application.class, args);
+        // Lấy ra bean SimpleBean trong Context
+        SimpleBean simpleBean = context.getBean(SimpleBean.class);
+        // In ra màn hình
+        System.out.println("Simple Bean Example: " + simpleBean.toString());
     }
 
 }
