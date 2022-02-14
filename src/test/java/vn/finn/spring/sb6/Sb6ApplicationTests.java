af02eb97e56ebb525838deb55c62dd15608cccdd
finn-01 <nguyensam576@gmail.com>
2022-02-14 11:03:44 +0700

    create app

---
+++ src/test/java/vn/finn/spring/sb6/Sb6ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.sb6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
